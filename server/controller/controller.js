2f100b1f09b7ab2e714f0c82cd315eb6f4e1c7f9
“Raja <rajadahri11@gmail.com>
2023-03-05 20:30:23 +0500

    created APIS

---
+++ server/controller/controller.js
@@ -0,0 +1,24 @@
+const Userdb = require('../model/model');
+
+// Create and Save a new User
+exports.create = (req, res)=>{
+    consol.log("create");
+}
+
+// Retrieve all Users from the database.
+
+exports.find = (req, res)=>{
+    consol.log("find");
+}
+
+// Find a single User with an id
+
+exports.update = (req, res)=>{
+    consol.log("update");
+}
+
+// Update a User by the id in the request
+
+exports.delete = (req, res)=>{
+    consol.log("delete");
+}
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const services = require('../services/render')
+const controller = require('../controller/controller')
 const route = express.Router()
 
 
@@ -9,5 +10,11 @@ route.get('/', services.homeRoutes);
 route.get('/add-user', services.add_user);
 //  Updating user data in database
 route.get('/update_user', services.update_user);
+
+// API
+route.post('api/users', controller.create);
+route.get('api/users', controller.find);
+route.put('api/users/:id', controller.update);
+route.delete('api/users/:id', controller.delete);
 
 module.exports = route;
